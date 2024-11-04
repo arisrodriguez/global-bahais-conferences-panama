159c6b8fa5985b2670f68e35a446f2ff4d02e11d
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2024-11-04 11:05:57 -0500

    cambios en el codigo

--- src/components/Conferences.jsx
+++ src/components/Conferences.jsx
@@ -4,7 +4,7 @@ export const Conferences = () => {
     return (
         <div className="flex justify-center items-center">
             <div className="text-center max-w-[1000px] md:w-[90%] px-[50px] md:px-0  py-[40px] md:py-[90px]">
-                <h1 className="font-extrabold text-[30px] pb-[30px] text-blue-light md:text-[35px]">Conferencias para la Transformación Colectiva</h1>
+                <h1 className="font-extrabold text-3xl pb-[30px] text-blue-light md:text-4xl">Conferencias para la Transformación Colectiva</h1>
                 <p className="pb-[40px] text-xl md:text-2xl">¡Bienvenidos a nuestras conferencias en diversas ciudades de Panamá! Este evento inclusivo reunirá a personas de todas las edades, orígenes y culturas. Las familias son especialmente bienvenidas, con programas diseñados para niños (5-11 años) y prejóvenes (12-15 años). No te pierdas la oportunidad de compartir, aprender y crecer juntos. ¡Te esperamos!</p>
 
                 <NextConferences />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from '../assets/images/logo-bahai.svg'
 
 export const Header = () => {
     return (
-        <header className="flex items-center justify-center gap-5 h-[100px]  bg-blue-light">
+        <header className="flex items-center justify-center gap-5 h-[100px] bg-blue-light">
             <div className='flex items-center justify-center gap-5'>
                 <img className='' src={logo} alt="Logo" />
                 <h1 className="text-[20px] sm:20px md:text-[28px] font-bold uppercase text-white">CONFERENCIAS DE PANAMá</h1>
