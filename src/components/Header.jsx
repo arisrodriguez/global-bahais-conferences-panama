5224c74e0055bcb107306bc68905666187160fcc
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2024-11-04 10:16:37 -0500

    Cambios en el header para mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import logo from '../assets/images/logo-bahai.svg'
 export const Header = () => {
     return (
         <header className="flex items-center justify-center gap-5 h-[100px]  bg-blue-light">
-            <div className='flex items-center justify-center gap-5 max-w-[1000px] '>
+            <div className='flex items-center justify-center gap-5'>
                 <img className='' src={logo} alt="Logo" />
-                <h1 className="text-[25px] md:text-[28px] font-bold uppercase text-white">CONFERENCIAS DE PANAMá</h1>
+                <h1 className="text-[20px] sm:20px md:text-[28px] font-bold uppercase text-white">CONFERENCIAS DE PANAMá</h1>
             </div>
         </header>
     )
