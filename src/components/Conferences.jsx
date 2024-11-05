47394798d222c945c8bd9197934e5c5c02f547bd
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2024-11-05 10:28:20 -0500

    Cambiamos la palabra evento por conferencia y agregamos habrán despues de bienvenidas.

--- src/components/Conferences.jsx
+++ src/components/Conferences.jsx
@@ -5,7 +5,7 @@ export const Conferences = () => {
         <div className="flex justify-center items-center">
             <div className="text-center max-w-[1000px] md:w-[90%] px-[50px] md:px-0 pt-10 ">
                 <h1 className="font-extrabold text-3xl md:text-4xl pb-[30px] text-blue-light">Conferencias para la Transformación Colectiva</h1>
-                <p className="pb-[40px] text-xl md:text-2xl">¡Bienvenidos a nuestras conferencias en diversas ciudades de Panamá! Este evento inclusivo reunirá a personas de todas las edades, orígenes y culturas. Las familias son especialmente bienvenidas, con programas diseñados para niños (5-11 años) y prejóvenes (12-15 años). No te pierdas la oportunidad de compartir, aprender y crecer juntos. ¡Te esperamos!</p>
+                <p className="pb-[40px] text-xl md:text-2xl">¡Bienvenidos a nuestras conferencias en diversas ciudades de Panamá! Estas conferencia inclusiva reunirá a personas de todas las edades, orígenes y culturas. Las familias son especialmente bienvenidas, habrán programas diseñados para niños (5-11 años) y prejóvenes (12-15 años). No te pierdas la oportunidad de compartir, aprender y crecer juntos. ¡Te esperamos!</p>
             </div>
         </div>
     )
