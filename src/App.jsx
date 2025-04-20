ce570492020ae37b60519490aaffebdd13604377
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2025-04-19 21:54:23 -0500

    fix: corregido import incorrecto de Header por uso de mayúsculas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Header } from './Components/Header'
+import { Header } from './components/Header'
 import { Video } from './components/Video'
 import { Purpose } from './components/Purpose'
 import { Banner } from './components/Banner'
