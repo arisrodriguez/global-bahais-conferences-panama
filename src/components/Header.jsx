b06ce85553affdb2a9241292230aad875c06f4fb
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2025-04-21 16:04:59 -0500

    Añadimos width y height explícitos al logo del header para mejorar CLS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <header className="flex items-center justify-center gap-5 h-[100px] bg-blue-light">
             <div className='flex items-center justify-center gap-5'>
-                <img className='' src={logo} alt="Logo" />
+                <img className='w-[50px] h-[50px]' src={logo} alt="Logo" width="50px" height="50px" />
                 <h1 className="text-[20px] sm:20px md:text-[28px] font-bold uppercase text-white">Conferencias Bahá'ís de Panamá</h1>
             </div>
         </header>
