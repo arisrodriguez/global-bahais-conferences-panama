352fcadd3f00e0577768b3a174c2cc5aea6c8fc0
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2025-04-21 11:05:35 -0500

    Quitamos la etiqueta Iframe de youtube por la libreria youtube-lite y ajustamos el tamaño del video para desktop

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,14 +1,14 @@
+import '@justinribeiro/lite-youtube';
+
 export const Video = () => {
     return (
-        <div className="flex justify-center items-center py-[30px]">
-            <iframe
-                className="w-full h-[300px] sm:h-[300px] md:h-[500px] lg:h-[500px]"
-                src="https://www.youtube.com/embed/58e6o4kp3Eg"
-                frameBorder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="YouTube video player"
-            ></iframe>
+        <div className="flex justify-center items-center py-[30px] mb-[30px]">
+            <div className='w-full max-w-[1000px] aspect-video">'>
+                <lite-youtube
+                    videoid="58e6o4kp3Eg"
+                    className="w-full h-full block"
+                ></lite-youtube>
+            </div>
         </div>
     )
 }
